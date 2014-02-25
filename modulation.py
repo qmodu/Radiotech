c14e4536e46daa760829424919373c1a6fcf8d38
Alexander Teleshov <alexander.teleshov@gmail.com>
2014-02-25 14:06:18 +0300

    add labels
    add labels

--- modulation.py
+++ modulation.py
@@ -4,7 +4,7 @@ import math  #Для sin(), cos()
 from numpy import * #Для функции arange(), функция поддерживает float
 #####################################
 
-Tm=1.0
+Tm=2.0
 Fd=500.0
 FFTL=8192
 
@@ -15,7 +15,11 @@ A=1 #Амплитуда исходного сигнала
 S=[A*math.sin(w*x) for x in arange(0,Tm,(1.0/Fd))] 
 
 #Делим область вывода на 3 строки, 1 столбец, выводим в первую строку (область)
-pylab.subplot (4, 1, 1) 
+pylab.subplot (7, 1, 1)
+pylab.xlabel('Time (s)')
+pylab.ylabel('Amplitude')
+pylab.title('Source signal')
+pylab.grid(True)
 pylab.plot(arange(0,Tm,(1.0/Fd)),S)
 print (len(arange(0,Tm,(1.0/Fd))))
 ######################################
@@ -33,21 +37,28 @@ M=[A1*math.sin(w1*x) for x in arange(0,Tm,(1.0/Fd))]
 ################   Модулируем  #######################
 AM=[A1*(1+A*math.sin(w*x))*math.sin(w1*x) for x in arange(0,Tm,(1.0/Fd))]
 #Делим область вывода на 3 строки, 1 столбец, выводим в 3 строку (область)
-pylab.subplot (4, 1, 3)
+pylab.subplot (7, 1, 5)
 pylab.plot(arange(0,Tm,(1.0/Fd)),AM)
-
-
+pylab.title('Amplitude modulation')
+pylab.xlabel('Time (s)')
+pylab.ylabel('Amplitude')
+pylab.grid(True)
 FFT_AM=fft.rfft(AM,FFTL)
 FFT_S=fft.rfft(S,FFTL)
 #print b
 #print len(arange(0,Fd,(Fd/FFTL)))
 #print 'F'
 #print len(abs(b))
-pylab.subplot (4, 1, 4) 
+pylab.subplot (7, 1, 7) 
 pylab.plot(arange(0,((Fd/2)+(Fd/FFTL)),(Fd/FFTL)),abs(FFT_AM)/len(arange(0,Tm,(1.0/Fd))))
-
-pylab.subplot (4, 1, 2) 
+pylab.title('Spectrum of AM signal')
+pylab.xlabel('Frequency (Hz)')
+pylab.ylabel('Amplitude')
+pylab.grid(True)
+pylab.subplot (7, 1, 3)
 pylab.plot(arange(0,((Fd/2)+(Fd/FFTL)),(Fd/FFTL)),abs(FFT_S)/len(arange(0,Tm,(1.0/Fd))))
-
-
+pylab.title('Spectrum of source signal')
+pylab.xlabel('Frequency (Hz)')
+pylab.ylabel('Amplitude')
+pylab.grid(True)
 pylab.show()
