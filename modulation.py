f8bd17884c39b2a6a372e78d7ee324f481f0ab8a
Alexander Teleshov <alexander.teleshov@gmail.com>
2014-02-25 23:17:30 +0300

    Update modulation.py
    Add noise generator

--- modulation.py
+++ modulation.py
@@ -1,26 +1,33 @@
 # -*- coding: UTF-8 -*-
 import pylab #Для графиков
+import random
 import math  #Для sin(), cos()
 from numpy import * #Для функции arange(), функция поддерживает float
 #####################################
 
-Tm=2.0
-Fd=500.0
+Tm=1.0
+Fd=1000.0
 FFTL=8192
 
 w=2*math.pi*10 #Угловая частота исходного сигнала
-A=1 #Амплитуда исходного сигнала
+A=2 #Амплитуда исходного сигнала
 
 # Частота сигнала 10 Гц, рисуем график на участке 0-1с, создаем список значений
+Noise=[2*random.random() for x in arange(0,Tm,(1.0/Fd))]
+#print len(Noise)
 S=[A*math.sin(w*x) for x in arange(0,Tm,(1.0/Fd))] 
+#print len(S)
 
+#print list(map(lambda a, b: a + b, Noise,S))
 #Делим область вывода на 3 строки, 1 столбец, выводим в первую строку (область)
 pylab.subplot (7, 1, 1)
 pylab.xlabel('Time (s)')
 pylab.ylabel('Amplitude')
 pylab.title('Source signal')
 pylab.grid(True)
-pylab.plot(arange(0,Tm,(1.0/Fd)),S)
+pylab.plot(arange(0,Tm,(1.0/Fd)),list(map(lambda a, b: a + b, Noise,S)))
+
+
 print (len(arange(0,Tm,(1.0/Fd))))
 ######################################
 
@@ -38,13 +45,13 @@ M=[A1*math.sin(w1*x) for x in arange(0,Tm,(1.0/Fd))]
 AM=[A1*(1+A*math.sin(w*x))*math.sin(w1*x) for x in arange(0,Tm,(1.0/Fd))]
 #Делим область вывода на 3 строки, 1 столбец, выводим в 3 строку (область)
 pylab.subplot (7, 1, 5)
-pylab.plot(arange(0,Tm,(1.0/Fd)),AM)
+pylab.plot(arange(0,Tm,(1.0/Fd)),list(map(lambda a, b: a + b, Noise,AM)))
 pylab.title('Amplitude modulation')
 pylab.xlabel('Time (s)')
 pylab.ylabel('Amplitude')
 pylab.grid(True)
-FFT_AM=fft.rfft(AM,FFTL)
-FFT_S=fft.rfft(S,FFTL)
+FFT_AM=fft.rfft(list(map(lambda a, b: a + b, Noise,AM)),FFTL)
+FFT_S=fft.rfft(list(map(lambda a, b: a + b, Noise,S)),FFTL)
 #print b
 #print len(arange(0,Fd,(Fd/FFTL)))
 #print 'F'
