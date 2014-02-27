692ae44c16aab835cd74396aacb342ff1b91c029
Alexander Teleshov <alexander.teleshov@gmail.com>
2014-02-27 13:57:31 +0300

    Made world better

--- Lab1/modulation.py
+++ Lab1/modulation.py
@@ -1,82 +1,57 @@
-# -*- coding: UTF-8 -*-
-import pylab #Для графиков
 import math  #Для sin(), cos()
-from numpy import * #Для функции arange(), функция поддерживает float
-#####################################
+from numpy import * #Для функции arange(), функция поддерживает тип float для аргументов
+
+########## Входные данные ##############
 
 Tm=4.0 #Длительность сигнала
 Fd=6000.0 #Частота дискретизации
-FFTL=8192 #
+FFTL=8192 #Количество линий спектра
 F1=200.0 #Частота исходного сигнала
 F2=2000.0 #Частота несущей
+A=1 #Амплитуда исходного сигнала
+A1=5 #Амплитуда несущего сигнала
 
 w=2*math.pi*F1 #Угловая частота исходного сигнала
-A=1 #Амплитуда исходного сигнала
-
-# Частота сигнала 10 Гц, рисуем график на участке 0-1с, создаем список значений
-S=[A*math.sin(w*x) for x in arange(0,Tm,(1.0/Fd))] 
-
-#Делим область вывода на 3 строки, 1 столбец, выводим в первую строку (область)
-pylab.subplot (9, 1, 1)
-pylab.xlabel('Time (s)')
-pylab.ylabel('Amplitude')
-pylab.title('Source signal')
-pylab.grid(True)
-print len(arange(0,(3/F1),(1.0/Fd)))
-
-
-pylab.plot(arange(0,(3/F1),(1.0/Fd)),S[0:(int(3*Fd/F1))])
-
-
-print (len(arange(0,Tm,(1.0/Fd))))
-######################################
-
 w1=2*math.pi*F2 #Угловая частота несущей
-A1=5
 
-# Частота сигнала 100 Гц, рисуем график на участке 0-1с, создаем список значений
+#################   Формирование списков значений сигналов   ######################
+
+# Формируем список значений исходного сигнала, длительностью Tm, с шагом 1/Fd
+S=[A*math.sin(w*x) for x in arange(0,Tm,(1.0/Fd))]
+
+# Формируем список значений несущего сигнала сигнала, длительностью Tm, с шагом 1/Fd
 M=[A1*math.sin(w1*x) for x in arange(0,Tm,(1.0/Fd))]
 
-#Делим область вывода на 3 строки, 1 столбец, выводим во 2 строку (область)
-#pylab.subplot (4, 1, 2)
-#pylab.plot(arange(0,Tm,(1.0/Fd)),M)
 
 ################   Модулируем  #######################
 AM=[A1*(1+A*math.sin(w*x))*math.sin(w1*x) for x in arange(0,Tm,(1.0/Fd))]
-#Делим область вывода на 3 строки, 1 столбец, выводим в 3 строку (область)
-pylab.subplot (9, 1, 5)
 
-pylab.plot(arange(0,(10/F1),(1.0/Fd)),AM[0:(int(10*Fd/F1))])
-
-#pylab.plot(arange(0,Tm,(1.0/Fd)),AM)
-pylab.title('Amplitude modulation')
-pylab.xlabel('Time (s)')
-pylab.ylabel('Amplitude')
-pylab.grid(True)
+################ Быстрое преобразование Фурье #################
+# Выполняем FFT для списка значений исходного сигнала, несущего сигнала и амплитудо-модулировонного сигнала
 FFT_AM=fft.rfft(AM,FFTL)
 FFT_S=fft.rfft(S,FFTL)
 FFT_M=fft.rfft(M,FFTL)
-#print b
-#print len(arange(0,Fd,(Fd/FFTL)))
-#print 'F'
-#print len(abs(b))
-pylab.subplot (9, 1, 7) 
-pylab.plot(arange(0,((Fd/2)+(Fd/FFTL)),(Fd/FFTL)),abs(FFT_AM)/len(arange(0,Tm/3,(1.0/Fd))))
-pylab.title('Spectrum of AM signal')
-pylab.xlabel('Frequency (Hz)')
-pylab.ylabel('Amplitude')
-pylab.grid(True)
-pylab.subplot (9, 1, 3)
-pylab.plot(arange(0,((Fd/2)+(Fd/FFTL)),(Fd/FFTL)),abs(FFT_S)/len(arange(0,Tm/3,(1.0/Fd))))
-pylab.title('Spectrum of source signal')
-pylab.xlabel('Frequency (Hz)')
-pylab.ylabel('Amplitude')
-pylab.grid(True)
 
-pylab.subplot (9, 1, 9) 
-pylab.plot(arange(0,((Fd/2)+(Fd/FFTL)),(Fd/FFTL)),abs(FFT_M)/len(arange(0,Tm/3,(1.0/Fd))))
-pylab.title('Spectrum of AM signal')
-pylab.xlabel('Frequency (Hz)')
-pylab.ylabel('Amplitude')
-pylab.grid(True)
+#################  Определение функции построения графиков  #####################
+
+def plot_signal(x,y,title,labelx,labley,position):
+    pylab.subplot (9, 1, position)
+    pylab.plot(x,y)
+    pylab.title(title)
+    pylab.xlabel(labelx)
+    pylab.ylabel(labley)
+    pylab.grid(True)           
+
+#Построение исходного сигнала
+plot_signal(arange(0,(3/F1),(1.0/Fd)),S[0:(int(3*Fd/F1))],'Source signal','Time (s)','Amplitude',1)
+#Построение спектра исходного сигнала
+plot_signal(arange(0,((Fd/2)+(Fd/FFTL)),(Fd/FFTL)),abs(FFT_S)/len(arange(0,Tm/3,(1.0/Fd))),'Spectrum of source signal','Frequency (Hz)','Amplitude',3)
+#Построение АМ сигнала
+plot_signal(arange(0,(10/F1),(1.0/Fd)),AM[0:(int(10*Fd/F1))],'Amplitude modulation','Time (s)','Amplitude',5)
+#Построение спектра несущего сигнала
+plot_signal(arange(0,((Fd/2)+(Fd/FFTL)),(Fd/FFTL)),abs(FFT_M)/len(arange(0,Tm/3,(1.0/Fd))),'Spectrum of carrier signal','Frequency (Hz)','Amplitude',7)
+#Построение спектра АМ сигнала
+plot_signal(arange(0,((Fd/2)+(Fd/FFTL)),(Fd/FFTL)),abs(FFT_AM)/len(arange(0,Tm/3,(1.0/Fd))),'Spectrum of AM signal','Frequency (Hz)','Amplitude',9)
+
+#Отображение графиков
 pylab.show()
